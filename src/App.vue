591a124247941d50fc95f93b34be5f8014b13f0a
GuilhermeSchreiber7 <guilhermeschreiber2007@gmail.com>
2024-07-01 16:46:25 -0300

    começo

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,55 @@
 <script setup>
+import { ref } from 'vue'
 
+const Estados = [
+  { uf: 'AC', nome: 'Acre' },
+  { uf: 'AL', nome: 'Alagoas' },
+  { uf: 'AP', nome: 'Amapá' },
+  { uf: 'AM', nome: 'Amazonas' },
+  { uf: 'BA', nome: 'Bahia' },
+  { uf: 'CE', nome: 'Ceará' },
+  { uf: 'DF', nome: 'Distrito Federal' },
+  { uf: 'ES', nome: 'Espírito Santo' },
+  { uf: 'GO', nome: 'Goiás' },
+  { uf: 'MA', nome: 'Maranhão' },
+  { uf: 'MT', nome: 'Mato Grosso' },
+  { uf: 'MS', nome: 'Mato Grosso do Sul' },
+  { uf: 'MG', nome: 'Minas Gerais' },
+  { uf: 'PA', nome: 'Pará' },
+  { uf: 'PB', nome: 'Paraíba' },
+  { uf: 'PR', nome: 'Paraná' },
+  { uf: 'PE', nome: 'Pernambuco' },
+  { uf: 'PI', nome: 'Piauí' },
+  { uf: 'RJ', nome: 'Rio de Janeiro' },
+  { uf: 'RN', nome: 'Rio Grande do Norte' },
+  { uf: 'RS', nome: 'Rio Grande do Sul' },
+  { uf: 'RO', nome: 'Rondônia' },
+  { uf: 'RR', nome: 'Roraima' },
+  { uf: 'SC', nome: 'Santa Catarina' },
+  { uf: 'SP', nome: 'São Paulo' },
+  { uf: 'SE', nome: 'Sergipe' },
+  { uf: 'TO', nome: 'Tocantins' }
+]
+ const infos = ref({
+  Nome: '',
+  Sobrenome: '',
+  Email: '',
+  Cidade:'',
+  Estado: '',
+  CEP: '',
+  Hobbies: [],
+  LinguagemProg:'',
+ }) 
 
+ const showprofile = ref(false),
 </script>
 
 <template>
-
+<div class="containerForm">
+<main>
+  <h1>SEU PERFIL:</h1>
+</main>
+</div>
 </template>
 
 <style scoped>
